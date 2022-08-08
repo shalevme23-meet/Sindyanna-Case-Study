264552c9f2e59c164c82efcc10bf5c42bd48b4e8
mayarsi23-meet <mayarsinnokrot10@gmail.com>
2022-08-08 18:18:42 +0300

    idk

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def arsignup():
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
