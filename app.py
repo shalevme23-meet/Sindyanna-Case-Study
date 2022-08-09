73b672b97f3cb29acf2c8ad86f2fafddfab596e6
ShadanAb23-meet <109904715+ShadanAb23-meet@users.noreply.github.com>
2022-08-09 13:08:25 +0300

    a

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def arsignup():
 
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
